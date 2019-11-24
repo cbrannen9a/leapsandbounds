e0fad553a22c1f378ea1be6d9fa034204f1676f2
Chris Brannen <chris.brannen@skybettingandgaming.com>
2019-11-24 11:14:25 +0000

    added scale to block

--- src/components/block/index.js
+++ src/components/block/index.js
@@ -4,11 +4,12 @@ import { GEOMETRY, MATERIAL, TILE_SIZE } from "../../constants";
 const Block = ({
   position = [0, 0, 0],
   rotation = [0, 0, 0],
+  scale = [1, 1, 1],
   size = [TILE_SIZE, TILE_SIZE, TILE_SIZE],
   colour = "red"
 }) => {
   return (
-    <mesh rotation={rotation} castShadow position={position}>
+    <mesh rotation={rotation} castShadow position={position} scale={scale}>
       <boxBufferGeometry attach={GEOMETRY} args={size} />
       <meshBasicMaterial attach={MATERIAL} color={colour} />
     </mesh>
